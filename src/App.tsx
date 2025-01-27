5a9eebf24b24c99f8c70f9db062b0ce570351e68
Aaron Bassett <aaronbassett@gmail.com>
2025-01-27 15:39:19 +0000

    💄 ui: install dasyUI and apply styles to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,43 @@
 import { useState } from "react";
 import viteLogo from "/vite.svg";
+import daisyUiLogo from "./assets/daisyUI.svg";
 import reactLogo from "./assets/react.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev">
-          <img src={viteLogo} alt="Vite logo" />
-        </a>
-        <a href="https://react.dev">
-          <img src={reactLogo} alt="React logo" />
-        </a>
+    <div className="mockup-browser mx-auto mt-30 max-w-5/6 border border-base-400">
+      <div className="mockup-browser-toolbar">
+        <div className="input">https://github.com/aaronbassett/doctrinaire</div>
       </div>
-      <h1>Vite + React</h1>
-      <div>
-        <button type="button" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="grid place-content-center space-y-10 bg-base-200 py-10">
+        <div className="flex items-center justify-center gap-4">
+          <a href="https://vite.dev" className="h-20 flex-auto">
+            <img src={viteLogo} alt="Vite logo" className="h-full" />
+          </a>
+          <a href="https://react.dev" className="h-20 flex-auto">
+            <img src={reactLogo} alt="React logo" className="h-full" />
+          </a>
+          <a href="https://v5.daisyui.com/" className="h-20 flex-auto">
+            <img src={daisyUiLogo} alt="daisyUI logo" className="h-full" />
+          </a>
+        </div>
+        <h1 className="text-center font-bold text-5xl">Vite + React + daisyUI</h1>
+        <div className="flex flex-col items-center justify-center space-y-4">
+          <button
+            className="btn btn-secondary btn-lg"
+            type="button"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test HMR
+          </p>
+        </div>
       </div>
-      <p>Click on the Vite and React logos to learn more</p>
-    </>
+    </div>
   );
 }
 
